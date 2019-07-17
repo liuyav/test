9f5ddbda384f15aa29c0425bf40035e0e949bdbc
liuyav <2472479696@qq.com>
2019-07-17 16:45:17 +0800

    vuex

---
+++ vue/vuex/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    {{num}}
+  </div>
+</template>
+
+<script>
+
+import {mapState} from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  computed: {
+    ...mapState({
+      num
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/vuex/src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import index from './modules/index'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+    index
+  }
+})
+
+export default store;
+
+
